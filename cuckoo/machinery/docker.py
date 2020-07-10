88d38d2842ea7ac8af25d53d6da6070c641220e1
Brian Lechthaler <brianlechthaler@protonmail.ch>
2020-07-10 16:51:15 -0700

    fix docker API init
    move client instantiation to the function it's used in, also clean up commented-out debug we're not using anymore

--- cuckoo/machinery/docker.py
+++ cuckoo/machinery/docker.py
@@ -1,27 +1,20 @@
 #Import Docker API
-import docker as vm
+import docker
 
 #Import necessary cuckoo components
 from cuckoo.common.abstracts import Machinery
 from cuckoo.common.exceptions import CuckooMachineError
 
-#Instantiate Docker API client
-vmclient = vm.from_env()
-
-#Check whether or not we're connected (debug)
-#isup = vmclient.ping()
-#if isup == True:
-#	print("connection to docker daemon successful")
-#elif isup == False:
-#	print("ERR:connection to docker daemon failed")
-
 class Docker(Machinery):
 	def agentStart():  
-        	#create container, start it, and run the agent inside it
+		#Instantiate Docker API Client
+		vmclient = docker.DockerClient(base_url='unix://var/run/docker.sock')
+		#create container, start it, and run the agent inside it
 		vmclient.containers.run("cuckooagent", "python /opt/agent.py", remove=True, detach=False, auto_remove=True, ports={'8000/tcp':8000}, name="cuckooMachinery", tty=True, network_mode="host")
         def agentStop():
 		#Instantiate low-level api client
 		lowlevelapi = vm.APIClient(base_url='unix://var/run/docker.sock')
+		#Kill the container
 		lowlevelapi.kill("cuckooMachinery")
         def start(self, label):
         	try:
