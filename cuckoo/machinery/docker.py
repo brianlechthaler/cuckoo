f9878554bdb4e916f1ac3235b58e42bbc2c9c125
Brian Lechthaler <brianlechthaler@protonmail.ch>
2020-07-09 21:46:37 -0700

    consolidate init sequence
    turns out that last commit was useless. that's ok because this one probably wont be.

--- cuckoo/machinery/docker.py
+++ cuckoo/machinery/docker.py
@@ -2,26 +2,24 @@ import docker as vm
 from cuckoo.common.abstracts import Machinery
 from cuckoo.common.exceptions import CuckooMachineError
 
-#initialize docker api client
-def vminit():
-    try:
-        vmclient = vm.from_env()
-    except dockerClientError:
-        raise CuckooMachineError("FAIL: Error communicating with Docker Daemon (could not instantiate a client.) Ensure Docker is installed and your user has access.")    
-vminit()
+vmclient = vm.from_env()
+isup = vmclient.ping()
+if isup == True:
+	print("connection to docker daemon successful")
+elif isup == False:
+	print("ERR:connection to docker daemon failed")
 
 class Docker(Machinery):
-    def start(self, label):
-        try:
-            
-            revert(label)
-            start(label)
-            
-        except SomethingBadHappens:
-            raise CuckooMachineError("oops!")
+	    def start(self, label):
+        	try:
+	            	revert(label)
+        	    	start(label)
+        	except SomethingBadHappens:
+	            	raise CuckooMachineError("oops!")
 
-    def stop(self, label):
-        try:
-            stop(label)
-        except SomethingBadHappens:
-            raise CuckooMachineError("oops!")
+    	def stop(self, label):
+        	try:
+            		stop(label)
+        	except SomethingBadHappens:
+            		raise CuckooMachineError("oops!")
+
