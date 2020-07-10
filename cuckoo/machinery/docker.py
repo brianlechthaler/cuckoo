7eb3dcba20b88650f33f1c34120c4e0a8dbeefe5
Brian Lechthaler <brianlechthaler@protonmail.ch>
2020-07-09 19:34:42 -0700

    Update docker.py
    added docker initialization steps

--- cuckoo/machinery/docker.py
+++ cuckoo/machinery/docker.py
@@ -2,9 +2,18 @@ import docker as vm
 from cuckoo.common.abstracts import Machinery
 from cuckoo.common.exceptions import CuckooMachineError
 
+#initialize docker api client
+def vminit(self, label):
+    try:
+        client = vm.from_env()
+    except:
+        raise CuckooMachineError("FAIL: Error communicating with Docker Daemon (could not instantiate a client.) Ensure Docker is installed and your user has access.")    
+
+
 class Docker(Machinery):
     def start(self, label):
         try:
+            
             revert(label)
             start(label)
             
